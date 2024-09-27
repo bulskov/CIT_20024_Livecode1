406a927eea93e8a2747eb79342b48ca87ee50fb7
bulskov <bulskov@ruc.dk>
2024-09-27 11:51:19 +0200

    Adding StackShouldBeEmptyWhenCreated test

--- Stack.Testing/StackTests.cs
+++ Stack.Testing/StackTests.cs
@@ -3,8 +3,17 @@ namespace Stack.Testing;
 public class StackTests
 {
     [Fact]
-    public void Test1()
+    public void StackShouldBeEmptyWhenCreated()
     {
+        var stack = new Stack();
 
+        Assert.Equal(stack.Count, 0); 
     }
+
+}
+
+class Stack
+{
+
+    public int Count { get; set; }
 }
