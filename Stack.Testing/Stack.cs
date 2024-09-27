be966a087c196793f8dc8d336fd409e78f9692ac
bulskov <bulskov@ruc.dk>
2024-09-27 12:01:34 +0200

    Move the Scack class into it's own file

---
+++ Stack.Testing/Stack.cs
@@ -0,0 +1,21 @@
+﻿namespace Stack.Testing;
+
+public class Stack
+{
+    int[] _value = new int[2];
+
+    public int Count { get; private set; } = 0;
+
+    public void Push(int value)
+    {
+        _value[Count] = value;
+        Count++;
+    }
+
+    public int Pop()
+    {
+        var pos = Count - 1;
+        Count--;
+        return _value[pos];
+    }
+}
--- Stack.Testing/StackTests.cs
+++ Stack.Testing/StackTests.cs
@@ -57,23 +57,3 @@ public class StackTests
     }
 
 }
-
-class Stack
-{
-    int[] _value = new int[2];
-
-    public int Count { get; private set; } = 0;
-
-    public void Push(int value)
-    {
-        _value[Count] = value;
-        Count++;
-    }
-
-    public int Pop()
-    {
-        var pos = Count - 1;
-        Count--;
-        return _value[pos];
-    }
-}
